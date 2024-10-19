557ec2bf40faa4a99b9abebed349856698d0a404
Johan Puentes <puentespulidojohansebastian@gmail.com>
2024-10-19 17:34:09 -0500

    Utilidades fisica

--- src/proyectointegrador/Fisica.java
+++ src/proyectointegrador/Fisica.java
@@ -4,14 +4,119 @@ package proyectointegrador;
  *
  * @author puent
  */
-
 class Conversiones {
-    
+
+    public double convertirTemperatura(double valor, String unidadOrigen, String unidadDestino) {
+        // Convertir a Celsius
+        if (unidadOrigen.equalsIgnoreCase("f")) {
+            valor = (valor - 32) * 5 / 9;
+        } else if (unidadOrigen.equalsIgnoreCase("k")) {
+            valor = valor - 273.15;
+        }
+
+        // Convertir desde Celsius a la unidad de destino
+        if (unidadDestino.equalsIgnoreCase("f")) {
+            return (valor * 9 / 5) + 32;
+        } else if (unidadDestino.equalsIgnoreCase("k")) {
+            return valor + 273.15;
+        }
+
+        return valor; // Si la unidad de destino es Celsius
+    }
+
+    // Conversión de Velocidad
+    public  double convertirVelocidad(double valor, String unidadOrigen, String unidadDestino) {
+        // Convertir a metros por segundo (m/s)
+        if (unidadOrigen.equalsIgnoreCase("km/h")) {
+            valor = valor / 3.6;
+        } else if (unidadOrigen.equalsIgnoreCase("mph")) {
+            valor = valor * 0.44704;
+        } else if (unidadOrigen.equalsIgnoreCase("ft/s")) {
+            valor = valor * 0.3048;
+        }
+
+        // Convertir desde metros por segundo a la unidad de destino
+        if (unidadDestino.equalsIgnoreCase("km/h")) {
+            return valor * 3.6;
+        } else if (unidadDestino.equalsIgnoreCase("mph")) {
+            return valor / 0.44704;
+        } else if (unidadDestino.equalsIgnoreCase("ft/s")) {
+            return valor / 0.3048;
+        }
+
+        return valor; // Si la unidad de destino es m/s
+    }
+
+    // Conversión de Energía
+    public double convertirEnergia(double valor, String unidadOrigen, String unidadDestino) {
+        // Convertir a Joules (J)
+        if (unidadOrigen.equalsIgnoreCase("cal")) {
+            valor = valor * 4.184;
+        } else if (unidadOrigen.equalsIgnoreCase("kcal")) {
+            valor = valor * 4184;
+        } else if (unidadOrigen.equalsIgnoreCase("wh")) {
+            valor = valor * 3600;
+        }
+
+        // Convertir desde Joules a la unidad de destino
+        if (unidadDestino.equalsIgnoreCase("cal")) {
+            return valor / 4.184;
+        } else if (unidadDestino.equalsIgnoreCase("kcal")) {
+            return valor / 4184;
+        } else if (unidadDestino.equalsIgnoreCase("wh")) {
+            return valor / 3600;
+        }
+
+        return valor; // Si la unidad de destino es Joules
+    }
+
+    // Conversión de Fuerza
+    public double convertirFuerza(double valor, String unidadOrigen, String unidadDestino) {
+        // Convertir a Newtons (N)
+        if (unidadOrigen.equalsIgnoreCase("kgf")) {
+            valor = valor * 9.80665;
+        } else if (unidadOrigen.equalsIgnoreCase("lbf")) {
+            valor = valor * 4.44822;
+        }
+
+        // Convertir desde Newtons a la unidad de destino
+        if (unidadDestino.equalsIgnoreCase("kgf")) {
+            return valor / 9.80665;
+        } else if (unidadDestino.equalsIgnoreCase("lbf")) {
+            return valor / 4.44822;
+        }
+
+        return valor; // Si la unidad de destino es Newtons
+    }
+
+    // Conversión de Masa
+    public double convertirMasa(double valor, String unidadOrigen, String unidadDestino) {
+        // Convertir a Kilogramos (kg)
+        if (unidadOrigen.equalsIgnoreCase("g")) {
+            valor = valor * 0.001;
+        } else if (unidadOrigen.equalsIgnoreCase("lb")) {
+            valor = valor * 0.453592;
+        } else if (unidadOrigen.equalsIgnoreCase("oz")) {
+            valor = valor * 0.0283495;
+        }
+
+        // Convertir desde Kilogramos a la unidad de destino
+        if (unidadDestino.equalsIgnoreCase("g")) {
+            return valor / 0.001;
+        } else if (unidadDestino.equalsIgnoreCase("lb")) {
+            return valor / 0.453592;
+        } else if (unidadDestino.equalsIgnoreCase("oz")) {
+            return valor / 0.0283495;
+        }
+
+        return valor; // Si la unidad de destino es Kilogramos
+    }
+
     public double convertirLongitud(double valor, String unidadOrigen, String unidadDestino) {
         double valorEnMetros = valor * factorConversionAmetros(unidadOrigen.toLowerCase());
         return valorEnMetros * factorConversionDesdeMetros(unidadDestino.toLowerCase());
     }
-    
+
     private double factorConversionAmetros(String unidad) {
         switch (unidad.toLowerCase()) {
             case "m" -> {
@@ -38,10 +143,11 @@ class Conversiones {
             case "mi" -> {
                 return 1609.34;
             }
-            default -> throw new IllegalArgumentException("Unidad de origen no soportada: " + unidad);
+            default ->
+                throw new IllegalArgumentException("Unidad de origen no soportada: " + unidad);
         }
     }
-    
+
     private double factorConversionDesdeMetros(String unidad) {
         switch (unidad.toLowerCase()) {
             case "m": // metros
@@ -66,18 +172,17 @@ class Conversiones {
     }
 }
 
-
 class MRU {
 
-    String MRUVelocidad(double distanciaKM, double tiempo) {
-        return (distanciaKM + "/" + tiempo);
+    String velocidad(double distanciaKM, double tiempo) {
+        return (distanciaKM + " m" + "/" + tiempo + " s");
     }
 
-    double MRUDistancia(double velocidad, double tiempo) {
+    double distancia(double velocidad, double tiempo) {
         return velocidad * tiempo;
     }
 
-    double MRUTiempo(double distancia, double velocidadEnKM) {
+    double tiempo(double distancia, double velocidadEnKM) {
         return distancia / velocidadEnKM;
     }
 }
@@ -92,12 +197,12 @@ class MRUA {
         return (velocidadInicial * tiempo) + 1 / 2 * aceleracionMetros * (Math.pow(tiempo, 2));
     }
 
-    double aceleracion (double velocidadInicial,double velocidadFinal, double tiempo) {
-        return (velocidadFinal-velocidadInicial)/tiempo;
+    double aceleracion(double velocidadInicial, double velocidadFinal, double tiempo) {
+        return (velocidadFinal - velocidadInicial) / tiempo;
     }
-    
-    double velocidadFinalConDistancia(double velocidadInicial,double aceleracionMetros,double distanciaMetros) {
-        return Math.sqrt(Math.pow(velocidadInicial,2)+2*aceleracionMetros*distanciaMetros);
+
+    double velocidadFinalConDistancia(double velocidadInicial, double aceleracionMetros, double distanciaMetros) {
+        return Math.sqrt(Math.pow(velocidadInicial, 2) + 2 * aceleracionMetros * distanciaMetros);
     }
 }
 
@@ -105,7 +210,4 @@ public class Fisica extends Conversiones {
     static MRUA mrua = new MRUA();
     static MRU mru = new MRU();
     static Conversiones conversiones = new Conversiones();
-    static void main() {
-       
-    }
 }
